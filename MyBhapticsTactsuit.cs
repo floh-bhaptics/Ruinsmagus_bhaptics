c1555e2b767bf1f5ffcddfa51cf8abfd96e83bc6
floh-bhaptics <floh@fahrenbergers.de>
2022-10-18 11:24:44 +0200

    Add project files.

---
+++ MyBhapticsTactsuit.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using MelonLoader;
+
+namespace MyBhapticsTactsuit
+{
+    public class TactsuitVR
+    {
+        /* A class that contains the basic functions for the bhaptics Tactsuit, like:
+         * - A Heartbeat function that can be turned on/off
+         * - A function to read in and register all .tact patterns in the bHaptics subfolder
+         * - A logging hook to output to the Melonloader log
+         * - 
+         * */
+        public bool suitDisabled = true;
+        public bool systemInitialized = false;
+        // Event to start and stop the heartbeat thread
+        private static ManualResetEvent HeartBeat_mrse = new ManualResetEvent(false);
+        private static ManualResetEvent SlowHeartBeat_mrse = new ManualResetEvent(false);
+        // dictionary of all feedback patterns found in the bHaptics directory
+        public Dictionary<String, FileInfo> FeedbackMap = new Dictionary<String, FileInfo>();
+
+        private static bHapticsLib.RotationOption defaultRotationOption = new bHapticsLib.RotationOption(0.0f, 0.0f);
+
+        public void HeartBeatFunc()
+        {
+            while (true)
+            {
+                // Check if reset event is active
+                HeartBeat_mrse.WaitOne();
+                bHapticsLib.bHapticsManager.PlayRegistered("HeartBeat");
+                Thread.Sleep(600);
+            }
+        }
+        public void SlowHeartBeatFunc()
+        {
+            while (true)
+            {
+                // Check if reset event is active
+                SlowHeartBeat_mrse.WaitOne();
+                bHapticsLib.bHapticsManager.PlayRegistered("HeartBeatSlow");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public TactsuitVR()
+        {
+            LOG("Initializing suit");
+            if (!bHaptics.WasError)
+            {
+                suitDisabled = false;
+            }
+            RegisterAllTactFiles();
+            LOG("Starting HeartBeat thread...");
+            Thread HeartBeatThread = new Thread(HeartBeatFunc);
+            HeartBeatThread.Start();
+            Thread SlowHeartBeatThread = new Thread(SlowHeartBeatFunc);
+            SlowHeartBeatThread.Start();
+        }
+
+        public void LOG(string logStr)
+        {
+#pragma warning disable CS0618 // remove warning that the logger is deprecated
+            MelonLogger.Msg(logStr);
+#pragma warning restore CS0618
+        }
+
+
+
+        void RegisterAllTactFiles()
+        {
+            // Get location of the compiled assembly and search through "bHaptics" directory and contained patterns
+            string configPath = Directory.GetCurrentDirectory() + "\\Mods\\bHaptics";
+            DirectoryInfo d = new DirectoryInfo(configPath);
+            FileInfo[] Files = d.GetFiles("*.tact", SearchOption.AllDirectories);
+            for (int i = 0; i < Files.Length; i++)
+            {
+                string filename = Files[i].Name;
+                string fullName = Files[i].FullName;
+                string prefix = Path.GetFileNameWithoutExtension(filename);
+                // LOG("Trying to register: " + prefix + " " + fullName);
+                if (filename == "." || filename == "..")
+                    continue;
+                string tactFileStr = File.ReadAllText(fullName);
+                try
+                {
+                    bHapticsLib.bHapticsManager.RegisterPatternFromJson(prefix, tactFileStr);
+                    LOG("Pattern registered: " + prefix);
+                }
+                catch (Exception e) { LOG(e.ToString()); }
+
+                FeedbackMap.Add(prefix, Files[i]);
+            }
+            systemInitialized = true;
+        }
+
+        public void PlaybackHaptics(String key, float intensity = 1.0f, float duration = 1.0f)
+        {
+            //LOG("Trying to play");
+            if (FeedbackMap.ContainsKey(key))
+            {
+                //LOG("ScaleOption");
+                bHapticsLib.ScaleOption scaleOption = new bHapticsLib.ScaleOption(intensity, duration);
+                //LOG("Submit");
+                bHapticsLib.bHapticsManager.PlayRegistered(key, key, scaleOption, defaultRotationOption);
+                // LOG("Playing back: " + key);
+            }
+            else
+            {
+                LOG("Feedback not registered: " + key);
+            }
+        }
+
+        public void PlayBackHit(String key, float xzAngle, float yShift)
+        {
+            // two parameters can be given to the pattern to move it on the vest:
+            // 1. An angle in degrees [0, 360] to turn the pattern to the left
+            // 2. A shift [-0.5, 0.5] in y-direction (up and down) to move it up or down
+            bHapticsLib.ScaleOption scaleOption = new bHapticsLib.ScaleOption(1f, 1f);
+            bHapticsLib.RotationOption rotationOption = new bHapticsLib.RotationOption(xzAngle, yShift);
+            bHapticsLib.bHapticsManager.PlayRegistered(key, key, scaleOption, rotationOption);
+        }
+
+        public void GetItem(bool isRightHand)
+        {
+            string postfix = "_L";
+            if (isRightHand) { postfix = "_R"; }
+
+            string keyWand = "GetWand" + postfix;
+
+            PlaybackHaptics(keyWand);
+        }
+
+        public void CastSpell(string spellName, bool isRightHand, float intensity = 1.0f)
+        {
+            float duration = 1.0f;
+            var scaleOption = new bHapticsLib.ScaleOption(intensity, duration);
+            var rotationFront = new bHapticsLib.RotationOption(0f, 0f);
+            string postfix = "_L";
+            if (isRightHand) { postfix = "_R"; }
+
+            string keyHand = "Spell" + spellName + "Hand" + postfix;
+            string keyArm = "Spell" + spellName + "Arm" + postfix;
+            string keyVest = "Spell" + spellName + "Vest" + postfix;
+            bHapticsLib.bHapticsManager.PlayRegistered(keyHand, keyHand, scaleOption, rotationFront);
+            bHapticsLib.bHapticsManager.PlayRegistered(keyArm, keyArm, scaleOption, rotationFront);
+            bHapticsLib.bHapticsManager.PlayRegistered(keyVest, keyVest, scaleOption, rotationFront);
+        }
+
+        public void HeadShot(String key, float hitAngle)
+        {
+            // I made 4 patterns in the Tactal for fron/back/left/right headshots
+            if (bHapticsLib.bHapticsManager.IsDeviceConnected(bHapticsLib.PositionID.Head))
+            {
+                if ((hitAngle < 45f) | (hitAngle > 315f)) { PlaybackHaptics("Headshot_F"); }
+                if ((hitAngle > 45f) && (hitAngle < 135f)) { PlaybackHaptics("Headshot_L"); }
+                if ((hitAngle > 135f) && (hitAngle < 225f)) { PlaybackHaptics("Headshot_B"); }
+                if ((hitAngle > 225f) && (hitAngle < 315f)) { PlaybackHaptics("Headshot_R"); }
+            }
+            // If there is no Tactal, just forward to the vest  with angle and at the very top (0.5)
+            else { PlayBackHit(key, hitAngle, 0.5f); }
+        }
+
+        public void StartHeartBeat()
+        {
+            HeartBeat_mrse.Set();
+        }
+
+        public void StopHeartBeat()
+        {
+            HeartBeat_mrse.Reset();
+        }
+        public void StartHeartBeatSlow()
+        {
+            SlowHeartBeat_mrse.Set();
+        }
+
+        public void StopHeartBeatSlow()
+        {
+            SlowHeartBeat_mrse.Reset();
+        }
+
+        public bool IsPlaying(String effect)
+        {
+            return bHapticsLib.bHapticsManager.IsPlaying(effect);
+        }
+
+        public void StopHapticFeedback(String effect)
+        {
+            bHapticsLib.bHapticsManager.StopPlaying(effect);
+        }
+
+        public void StopAllHapticFeedback()
+        {
+            StopThreads();
+            foreach (String key in FeedbackMap.Keys)
+            {
+                bHapticsLib.bHapticsManager.StopPlaying(key);
+            }
+        }
+
+        public void StopThreads()
+        {
+            // Yes, looks silly here, but if you have several threads like this, this is
+            // very useful when the player dies or starts a new level
+            StopHeartBeat();
+        }
+
+
+    }
+}
---
+++ Ruinsmagus_bhaptics.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MelonLoader;
+using HarmonyLib;
+using MyBhapticsTactsuit;
+
+using App.Players.BattleSystems.Magics;
+using App.Equipments.Gauntlets.Animations;
+
+namespace Ruinsmagus_bhaptics
+{
+    public class Ruinsmagus_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+        public static bool rightHanded = true;
+
+        [Obsolete]
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+
+
+        [HarmonyPatch(typeof(HandedInputSelector), "SetActiveController", new Type[] { typeof(OVRInput.Controller) })]
+        public class bhaptics_ChangeHandedness
+        {
+            [HarmonyPostfix]
+            public static void Postfix(OVRInput.Controller c)
+            {
+                rightHanded = ((c == OVRInput.Controller.RHand) || (c == OVRInput.Controller.RTouch));
+            }
+        }
+
+
+        [HarmonyPatch(typeof(PlayerMagicEmittersManager), "OnEmitAtEmitter", new Type[] { typeof(App.Magics.MasterData.MagicMasterData) })]
+        public class bhaptics_CastPrimarySpell
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.CastSpell("Fire", true);
+            }
+        }
+        
+
+        [HarmonyPatch(typeof(App.Players.AvatarMangers.Cartridges.HandCartridgesManager), "PlayReloadSoundEffects", new Type[] {  })]
+        public class bhaptics_ReloadCartridges
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.LOG("bhaptics_ReloadSoundEffect");
+                tactsuitVr.PlaybackHaptics("HeartBeat");
+            }
+        }
+
+        [HarmonyPatch(typeof(App.Items.ConsumableItems.BaseConsumableItem), "Consume", new Type[] { })]
+        public class bhaptics_ConsumeItem
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.PlaybackHaptics("Eating");
+            }
+        }
+
+        [HarmonyPatch(typeof(App.Players.PlayerCore), "OnDie", new Type[] { })]
+        public class bhaptics_PlayerDies
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.StopThreads();
+            }
+        }
+
+        
+        [HarmonyPatch(typeof(App.Equipments.Shields.Views.ShieldBodyView), "OnGuardSucceed", new Type[] {  })]
+        public class bhaptics_ShieldAttacked
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.PlaybackHaptics("RecoilBlockVest_L");
+            }
+        }
+
+        [HarmonyPatch(typeof(App.Players.BattleSystems.PlayerDamageManager), "CalculatePlayerDamageStateAndPlayEffect", new Type[] { typeof(int), typeof(bool) })]
+        public class bhaptics_TakeDamage
+        {
+            [HarmonyPostfix]
+            public static void Postfix(App.Players.BattleSystems.PlayerDamageManager __instance, int currentHealth, bool isIncrease)
+            {
+                //if (currentHealth * 1.0f <= 0.25f * __instance) tactsuitVr.StartHeartBeat();
+                //else tactsuitVr.StopHeartBeat();
+                tactsuitVr.LOG("Health: " + currentHealth.ToString());
+                if (isIncrease) return;
+                tactsuitVr.PlaybackHaptics("Impact");
+            }
+        }
+
+    }
+}
